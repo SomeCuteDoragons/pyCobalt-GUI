7155a65890acf8cc0039f1a7305968100b65c2c2
BrunoAncic31 <bruno.ancic.31@gmail.com>
2023-09-09 15:52:21 +0200

    Special thanks to mikhail from cobaltcord for the code makeover

--- Cobalt_GUI.py
+++ Cobalt_GUI.py
@@ -2,35 +2,49 @@ import requests
 import json
 import tkinter as tk
 from tkinter import filedialog
-import tkinter.ttk as ttk
 import os
-import re
+
+# Special thanks to mikhail from cobaltcord for the code makeover
+
+# Global variables / magic values
+VIDEO_CODECS = {
+    "vp9": ".webm",
+    "av1": ".mp4",
+    "h264": ".mp4"
+}
+COBALT_API_URL = "https://co.wuk.sh/api/json"
+
+KIBIBYTE = 1024
+MEBIBYTE = KIBIBYTE * KIBIBYTE
+
+CHUNK_SIZE = KIBIBYTE * 8
 
 # Define variables for the request body values
 video_url = ""
-vCodec = "h264"
-vQuality = "720"
-aFormat = "mp3"  # Please don't set to best :)
-isAudioOnly = False
-isNoTTWatermark = False
-isTTFullAudio = False
-isAudioMuted = False
-dubLang = False
+video_codec = "h264"
+video_quality = "720"
+audio_format = "mp3"  # Ok you can set it to best now
+is_audio_only = False
+disable_tiktok_watermark = False
+is_tiktok_full_audio = False
+is_audio_muted = False
+dub_lang = False
 
-# Function to extract the file extension from the Content-Disposition header
-def extract_file_extension(response):
-    content_disposition = response.headers.get('Content-Disposition')
-    if content_disposition:
-        match = re.search(r'filename="(.+)"', content_disposition)
-        if match:
-            filename = match.group(1)
-            _, file_extension = os.path.splitext(filename)
-            return file_extension.lower()
-    return None
+
+# Function that makes the size in bytes as an human-readable string
+def humanize_size(size_bytes):
+    if size_bytes < KIBIBYTE:
+        return f"{size_bytes} bytes"
+
+    if size_bytes > KIBIBYTE and MEBIBYTE > size_bytes:
+        return f"{size_bytes / KIBIBYTE} KiB"
+
+    return f"{size_bytes / MEBIBYTE} MiB"
+
 
 # Function to handle the download button click
 def download_video():
-    global video_url, vCodec, vQuality, aFormat, isAudioOnly, isNoTTWatermark, isTTFullAudio, isAudioMuted, dubLang
+    global video_url, video_codec, video_quality, audio_format, is_audio_only, disable_tiktok_watermark, is_tiktok_full_audio, is_audio_muted, dub_lang
 
     # Get the video URL from the input field
     video_url = video_url_entry.get()
@@ -40,26 +54,7 @@ def download_video():
         return
 
     # Determine the file extension based on the selected video codec or audio format
-    if not isAudioOnly:
-        if vCodec.lower() in ("h264", "av1"):
-            file_extension = ".mp4"
-        elif vCodec.lower() == "vp9":
-            file_extension = ".webm"
-        else:
-            print(f"Invalid vCodec value: {vCodec}")
-            return
-    else:
-        if aFormat.lower() == "mp3":
-            file_extension = ".mp3"
-        elif aFormat.lower() == "ogg":
-            file_extension = ".ogg"
-        elif aFormat.lower() == "wav":
-            file_extension = ".wav"
-        elif aFormat.lower() == "opus":
-            file_extension = ".opus"
-        else:
-            print(f"Invalid aFormat value: {aFormat}")
-            return
+    file_extension = VIDEO_CODECS[video_codec] if not is_audio_only else "." + audio_format
 
     # Use the file dialog to choose the download location
     file_path = filedialog.asksaveasfilename(
@@ -71,88 +66,95 @@ def download_video():
         print("Download canceled.")
         return
 
-    # Define the base URL for the Cobalt API
-    base_url = "https://co.wuk.sh"
-
-    # Define the endpoint for the POST request
-    endpoint = "/api/json"
-
-    # Construct the full API URL
-    api_url = base_url + endpoint
-
     # Define the request body parameters using the variables
     request_body = {
         "url": video_url,
-        "vCodec": vCodec,
-        "vQuality": vQuality,
-        "aFormat": aFormat,
-        "isAudioOnly": isAudioOnly,
-        "isNoTTWatermark": isNoTTWatermark,
-        "isTTFullAudio": isTTFullAudio,
-        "isAudioMuted": isAudioMuted,
-        "dubLang": dubLang
+        "vCodec": video_codec,
+        "vQuality": video_quality,
+        "aFormat": audio_format,
+        "isAudioOnly": is_audio_only,
+        "isNoTTWatermark": disable_tiktok_watermark,
+        "isTTFullAudio": is_tiktok_full_audio,
+        "isAudioMuted": is_audio_muted,
+        "dubLang": dub_lang
     }
 
     # Define the headers with the "Accept" header set to "application/json"
-    headers = {
-        "Accept": "application/json"
-    }
+    headers = {"accept": "application/json"}
 
     # Send a POST request to the API with the headers
-    response = requests.post(api_url, json=request_body, headers=headers)
+    response = requests.post(COBALT_API_URL, json=request_body, headers=headers)
 
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        data = response.json()
-
-        # Process the JSON response here
-        print("Response Data:", json.dumps(data, indent=4))
-
-        # Check if the response contains a file URL
-        if 'url' in data:
-            file_url = data['url']
-
-            # Extract the directory path from the chosen file path
-            download_dir = os.path.dirname(file_path)
-
-            # Ensure that the download directory exists, create it if necessary
-            if not os.path.exists(download_dir):
-                os.makedirs(download_dir)
-
-            # Download and save the file to the chosen location
-            file_response = requests.get(file_url)
-
-            if file_response.status_code == 200:
-                with open(file_path, 'wb') as file:
-                    file.write(file_response.content)
-
-                print(f"File '{file_path}' has been downloaded and saved.")
-            else:
-                print(f"Error downloading file: Status code {file_response.status_code}")
-                print("Response Content:", file_response.text)
-        else:
-            print("No file URL found in the API response.")
-    else:
+    if response.status_code != 200:
         print(f"Error: Request failed with status code {response.status_code}")
         print("Response Content:", response.text)
 
+        return
 
-# Create the tkinter window
-window = tk.Tk()
-window.title("pyCobalt Downloader")
-window.geometry("640x480")
-window.resizable(False, False)
-window.attributes('-topmost', 1)
+    data = response.json()
 
-# Label and Entry for Video URL
-video_url_label = ttk.Label(window, text="Enter Video URL:")
-video_url_label.pack()
-video_url_entry = ttk.Entry(window)
-video_url_entry.pack()
+    # Process the JSON response here
+    print("Response Data:", json.dumps(data, indent=4))
 
-# Download Button
-download_button = ttk.Button(window, text="Download Media", command=download_video)
-download_button.pack()
+    if "url" not in data:
+        print("No file URL found in the API response.")
 
-# Start the tkinter main loop
-window.mainloop()
+        return
+
+    file_url = data["url"]
+
+    # Extract the directory path from the chosen file path
+    download_dir = os.path.dirname(file_path)
+
+    # Ensure that the download directory exists, create it if necessary
+    if not os.path.exists(download_dir):
+        os.makedirs(download_dir)
+
+    # Download and save the file to the chosen location
+    file_response = requests.get(file_url, stream=True)
+
+    if file_response.status_code != 200:
+        print(f"Error downloading file: Status code {file_response.status_code}")
+        print("Response Content:", file_response.text)
+
+        return
+
+    current_file_size = 0
+
+    # Downloading the file
+    with open(file_path, "wb") as file:
+        for chunk in file_response.iter_content(chunk_size=CHUNK_SIZE):
+            file.write(chunk)
+            current_file_size = file.tell()
+
+            if current_file_size % MEBIBYTE == 0:
+                current_file_size = humanize_size(current_file_size)
+                print(f"Downloaded {current_file_size} of content.")
+
+    current_file_size = humanize_size(current_file_size)
+    print(f"File '{file_path}' has been downloaded and saved. File size is {current_file_size}")
+
+
+def main():
+    global video_url_entry
+
+    # Create the tkinter window
+    window = tk.Tk()
+    window.title("pyCobalt GUI")
+
+    # Label and Entry for Video URL
+    video_url_label = tk.Label(window, text="Enter Video URL:")
+    video_url_label.pack()
+    video_url_entry = tk.Entry(window)
+    video_url_entry.pack()
+
+    # Download Button
+    download_button = tk.Button(window, text="Download Media", command=download_video)
+    download_button.pack()
+
+    # Start the tkinter main loop
+    window.mainloop()
+
+
+if __name__ == "__main__":
+    main()
